5d01cea84264f142692d78036c5ff4788906aa05
João Marques <joaomarques.jm991@gmail.com>
2021-05-06 00:34:40 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.devsuperior.dsvendas.entities.Seller;
 
 public interface SellerRepository extends JpaRepository<Seller, Long>{
+	
 
 }
